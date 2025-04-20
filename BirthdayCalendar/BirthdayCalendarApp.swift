39b9205910c786dd3203242be85c0e03203e6b9e
Ola Lovholm <ola.lovholm@origo.oslo.kommune.no>
2025-04-20 13:44:01 +0200

    Initial Commit

---
+++ BirthdayCalendar/BirthdayCalendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  BirthdayCalendarApp.swift
+//  BirthdayCalendar
+//
+//  Created by Ola Loevholm on 20/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct BirthdayCalendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
